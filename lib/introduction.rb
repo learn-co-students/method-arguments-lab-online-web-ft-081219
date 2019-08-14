3d553229a7cb94e3478772ac527c6d1e326bda4a
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-14 06:01:20 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(string)
+  puts "Hi, my name is #{string}.\n"
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
